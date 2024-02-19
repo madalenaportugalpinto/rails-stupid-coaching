a777b33a40046c4aeac791b610d68c8e990a323f
Madalena Pinto <madalenaportugalpinto@gmail.com>
2024-02-19 12:00:28 +0000

    stupid coach

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
+
+# First page is a form with an input, where a user can type a question to ask the Coach
+# After submitting the form, the user is redirected to another page where
+# she/he will see her/his question and the coach answer.
